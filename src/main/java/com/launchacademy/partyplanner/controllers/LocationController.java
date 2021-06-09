e1d6e86e5809f789fd6a79738d052cd354d3c5fd
jessica_mach <Jessica.Mach@outlook.com>
2021-06-09 12:52:18 -0700

    Added new location form with validations

--- src/main/java/com/launchacademy/partyplanner/controllers/LocationController.java
+++ src/main/java/com/launchacademy/partyplanner/controllers/LocationController.java
@@ -3,12 +3,16 @@ package com.launchacademy.partyplanner.controllers;
 import com.launchacademy.partyplanner.models.Location;
 import com.launchacademy.partyplanner.services.LocationService;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -39,5 +43,20 @@ public class LocationController {
       model.addAttribute("location", showLocation.get());
     }
     return "locations/show";
+  }
+
+  @GetMapping("/new")
+  public String getNewLocationForm(@ModelAttribute Location location) {
+    return "locations/new";
+  }
+
+  @PostMapping
+  public String createLocation(@ModelAttribute @Valid Location location, BindingResult bindingResult) {
+    if(bindingResult.hasErrors()) {
+      return "locations/new";
+    } else {
+      locationService.save(location);
+      return "redirect:/locations";
+    }
   }
 }
